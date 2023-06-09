91de8b72ca1e65bcb5c52c360ae8093151156926
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-09 16:13:39 +0200

    automating tasks

---
+++ src/GameVisionTargetingModel/variables/__init__.py
@@ -0,0 +1,15 @@
+from src.GameVisionTargetingModel.variables.model_settings \
+    import                  \
+    get_input_set,          \
+    get_width,              \
+    get_channels,           \
+    get_height,             \
+    get_number_of_labels,   \
+    get_last_layer_multiplier
+
+from src.GameVisionTargetingModel.variables.model_settings \
+    import              \
+    set_width,          \
+    set_height,         \
+    set_channels,       \
+    set_number_of_labels
