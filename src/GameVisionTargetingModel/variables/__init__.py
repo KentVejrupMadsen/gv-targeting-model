40bc97544f82d657557d66b08172a287d98852a6
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-09 16:30:48 +0200

    removed
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- src/GameVisionTargetingModel/variables/__init__.py
+++ src/GameVisionTargetingModel/variables/__init__.py
@@ -8,8 +8,9 @@ from src.GameVisionTargetingModel.variables.model_settings \
     get_last_layer_multiplier
 
 from src.GameVisionTargetingModel.variables.model_settings \
-    import              \
-    set_width,          \
-    set_height,         \
-    set_channels,       \
-    set_number_of_labels
+    import                  \
+    set_width,              \
+    set_height,             \
+    set_channels,           \
+    set_number_of_labels,   \
+    set_last_layer_multiplier
